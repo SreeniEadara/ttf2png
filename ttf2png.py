476f4bfc0df6413419a0f74133e8b3cc48a339ef
SreeniEadara <sreeni.eadara@gmail.com>
2026-03-18 17:44:36 -0500

    WIP - change to func with single in/out, remove other code

--- ttf2png.py
+++ ttf2png.py
@@ -5,34 +5,16 @@ import subprocess
 
 from fontTools.ttLib import TTFont
 
-TEXTS_DIR = "texts"
-IMAGES_DIR = "images"
+def dump_png(font_path: str, font_size: int, text_path: str, image_path: str):
+    ttf = TTFont(font_path, 0, verbose=0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
+    for x in ttf["cmap"].tables:
+        for y in x.cmap.items():
+            char_unicode = chr(y[0])
+            char_utf8 = char_unicode.encode('utf_8')
+            char_name = y[1]
+            f = open(os.path.join(text_path, char_name + '.txt'), 'w')
+            f.write(char_utf8)
+            f.close()
+    ttf.close()
+    subprocess.call(["convert", "-font", font_path, "-pointsize", font_size, "-background", "rgba(0,0,0,0)", "label:@" + text_path, image_path])
 
-TTF_PATH = sys.argv[1]
-FONT_SIZE = sys.argv[2]
-TTF_NAME, TTF_EXT = os.path.splitext(os.path.basename(TTF_PATH))
-
-ttf = TTFont(TTF_PATH, 0, verbose=0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
-
-for d in [TEXTS_DIR, IMAGES_DIR]:
-    if not os.path.isdir(d):
-        os.mkdir(d)
-
-for x in ttf["cmap"].tables:
-    for y in x.cmap.items():
-        char_unicode = unichr(y[0])
-        char_utf8 = char_unicode.encode('utf_8')
-        char_name = y[1]
-        f = open(os.path.join(TEXTS_DIR, char_name + '.txt'), 'w')
-        f.write(char_utf8)
-        f.close()
-ttf.close()
-
-files = os.listdir(TEXTS_DIR)
-for filename in files:
-    name, ext = os.path.splitext(filename)
-    input_txt = TEXTS_DIR + "/" + filename
-    output_png = IMAGES_DIR + "/" + TTF_NAME + "_" + name + "_" + FONT_SIZE + ".png"
-    subprocess.call(["convert", "-font", TTF_PATH, "-pointsize", FONT_SIZE, "-background", "rgba(0,0,0,0)", "label:@" + input_txt, output_png])
-
-print("finished")
