7071ab1f976e6fdc5eb623cc8318c06f54c84173
SreeniEadara <sreeni.eadara@gmail.com>
2026-03-18 18:12:40 -0500

    fix args and only do single char

--- ttf2png.py
+++ ttf2png.py
@@ -5,16 +5,21 @@ import subprocess
 
 from fontTools.ttLib import TTFont
 
-def dump_png(font_path: str, font_size: int, text_path: str, image_path: str):
+def dump_png(char: str, font_path: str, point_size: int, text_path: str, image_path: str):
     ttf = TTFont(font_path, 0, verbose=0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
+    char_found = False
     for x in ttf["cmap"].tables:
         for y in x.cmap.items():
             char_unicode = chr(y[0])
+            if char_unicode != char:
+                continue
+            else:
+                char_found = True
             char_utf8 = char_unicode.encode('utf_8')
-            char_name = y[1]
-            f = open(os.path.join(text_path, char_name + '.txt'), 'w')
+            f = open(text_path, 'bw')
             f.write(char_utf8)
             f.close()
     ttf.close()
-    subprocess.call(["convert", "-font", font_path, "-pointsize", font_size, "-background", "rgba(0,0,0,0)", "label:@" + text_path, image_path])
-
+    if not char_found:
+        raise RuntimeError(f'Character {char} not in TTF {font_path}')
+    subprocess.call(["convert", "-font", font_path, "-pointsize", str(point_size), "-background", "rgba(0,0,0,0)", "label:@" + text_path, image_path])
